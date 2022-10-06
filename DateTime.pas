42f83fd3b741047c3c4cce82d043905e716ffeac
vladkach1 <vlad.vlad.pvv@gmail.com>
2022-10-06 14:41:52 +0300

    fix DateTime.pas

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,7 @@
 ﻿begin
-  
+  var q := DateTime.Now.Year;
+  if (((q mod 4) = 0) and ((q mod 100) <> 0)) or ((q mod 400) = 0) then
+    println('Високосный год')
+  else 
+    println('Не Високосный год');
 end.
